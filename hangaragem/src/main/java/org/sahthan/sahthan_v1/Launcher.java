e764bece4b79075f4ffef6354f9eb4d85faa649f
astrolia <nabreu.acad@gmail.com>
2026-06-10 23:31:34 -0300

    rodogit add hangaragem/!

--- hangaragem/src/main/java/org/sahthan/sahthan_v1/Launcher.java
+++ hangaragem/src/main/java/org/sahthan/sahthan_v1/Launcher.java
@@ -1,13 +1,14 @@
 package org.sahthan.sahthan_v1;
 
 import javafx.application.Application;
+import org.sahthan.sahthan_v1.application.LoginApplication;
 import org.sahthan.sahthan_v1.application.MenuApllication;
 
     public class Launcher {
     public static void main(String[] args) {
 
 
-        Application.launch(MenuApllication.class, args);
+        Application.launch(LoginApplication.class, args);
     }
 
 }
